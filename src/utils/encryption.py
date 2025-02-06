7ec17137f083666d7b32c19fca63643c84f7a6dd
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-06 23:10:37 +0800

    Update encryption.py

--- src/utils/encryption.py
+++ src/utils/encryption.py
@@ -1,1 +1,11 @@
+from cryptography.fernet import Fernet
 
+def encrypt_weights(weights):
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    encrypted_weights = f.encrypt(weights)
+    return encrypted_weights, key
+
+def decrypt_weights(encrypted_weights, key):
+    f = Fernet(key)
+    return f.decrypt(encrypted_weights)
