0d6013570dafb68551b76e30c4beb7874695cd82
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-06 23:20:55 +0800

    Update test_load.py

--- tests/performance/test_load.py
+++ tests/performance/test_load.py
@@ -1,1 +1,6 @@
+from locust import HttpUser, task
 
+class LoadTest(HttpUser):
+    @task(1)
+    def predict(self):
+        self.client.post("/predict", files={"image": open("data/raw/sample_01.jpg", "rb")})
