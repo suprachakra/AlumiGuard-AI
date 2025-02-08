edaad8529137bcfc7597142ab9bb5804e616fea5
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-09 01:38:08 +0800

    Update deploy_azure.sh

--- scripts/deploy_azure.sh
+++ scripts/deploy_azure.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+az login
+az acr login --name <acr-name>
+
+docker tag aluminum-defect-detection:latest <acr-name>.azurecr.io/aluminum-defect-detection:latest
+
+docker push <acr-name>.azurecr.io/aluminum-defect-detection:latest
+
+az aks get-credentials --resource-group <resource-group> --name <aks-cluster-name>
+
+kubectl apply -f kubernetes/deployment.yaml
