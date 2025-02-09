2cc8980d6d0a1435d09a7da4cb1d7f184b72f91b
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-09 20:03:10 +0800

    Update key_vault_integration.sh

--- scripts/key_vault_integration.sh
+++ scripts/key_vault_integration.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# Sample script to pull secrets from Azure Key Vault
 
+VAULT_NAME="myKeyVault"
+SECRET_NAME="JWT_SECRET_KEY"
+
+echo "Retrieving $SECRET_NAME from $VAULT_NAME..."
+SECRET_VALUE=$(az keyvault secret show --vault-name $VAULT_NAME --name $SECRET_NAME --query value -o tsv)
+
+export JWT_SECRET_KEY=$SECRET_VALUE
+echo "Exported JWT_SECRET_KEY from Key Vault."
