df74c8ad9d10bc0dc0b0a7ad693b424fae7ea555
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-09 20:05:03 +0800

    Update outputs.tf

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,1 +1,9 @@
+output "acr_login_server" {
+  description = "ACR Login Server"
+  value       = azurerm_container_registry.acr.login_server
+}
 
+output "aks_fqdn" {
+  description = "AKS FQDN"
+  value       = azurerm_kubernetes_cluster.aks.fqdn
+}
