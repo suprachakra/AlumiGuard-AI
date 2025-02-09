bed461160c7b02c1b2e96d58855e1faf1ac58d81
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-09 19:04:27 +0800

    Update config_loader.py

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -1,5 +1,8 @@
 import yaml
 
-def load_config(path):
-    with open(path) as f:
+def load_config(path='config/params.yaml'):
+    """
+    Loads configuration from a YAML file.
+    """
+    with open(path, 'r') as f:
         return yaml.safe_load(f)
